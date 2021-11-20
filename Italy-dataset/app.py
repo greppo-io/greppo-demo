d907652c4e88130e54f3538458aaffaa7c24409d
Adithya Krishnan <krishsandeep@gmail.com>
2021-11-20 23:38:30 +0100

    update Italy demo and readme.

---
+++ Italy-dataset/app.py
@@ -0,0 +1,38 @@
+from greppo import app
+import geopandas as gpd
+
+app.base_layer(
+    name="CartoDB Light",
+    visible=True,
+    url="https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/{z}/{x}/{y}@2x.png",
+    subdomains=None,
+    attribution='&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors | Carto Tile',
+)
+
+regions = gpd.read_file("./regions.geojson")
+roads = gpd.read_file("./roads.geojson")
+cities = gpd.read_file("./cities.geojson")
+
+app.overlay_layer(
+    regions,
+    title="Regions of Italy",
+    description="Polygons showing the boundaries of regions of Italy.",
+    style={"fillColor": "#74c476"},
+    visible=True,
+)
+
+app.overlay_layer(
+    roads,
+    title="Highways in Italy",
+    description="Lines showing the major highways in Italy.",
+    style={"fillColor": "#6a51a3"},
+    visible=True,
+)
+
+app.overlay_layer(
+    cities,
+    title="Cities of Italy",
+    description="Points showing the cities in Italy.",
+    style={"fillColor": "#de2d26"},
+    visible=True,
+)
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # greppo-demo
+
 A repository of demo apps built using Greppo. 
+
+## Quickstart
+
+1. Install `greppo` using:
+
+```shell
+$ pip install greppo
+```
+Note: It is recommended to install `greppo` inside a python environment.
+
+2. Go inside one of the demo apps folders in this repository and run the command:
+
+```shell
+$ greppo serve app.py
+```
+
+3. Follow the instructions shown.
